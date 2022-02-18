aa6471ef0a5034ab71d1aa62905741d1328e53ad
Sinta Anjelina <sinta.anjelina3930@gmail.com>
2022-02-18 22:08:32 +0700

    Example of markup React in Project 1

--- index.js
+++ index.js
@@ -1,72 +1,15 @@
-function Navbar() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-          <div className="offcanvas-header">
-            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Navbar</h5>
-            <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-          </div>
-          <div className="offcanvas-body">
-            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="offcanvasNavbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="offcanvasNavbarDropdown">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-            </ul>
-            <form className="d-flex">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </nav>
-  )
-}
-function Hello(props) {
-  return (
-    <h5>Hello, {props.name}! Nice to know you :-)</h5>
-  )
-}
-
-const navbar = (
-  <nav>
-    <h1> IOT - ID </h1>
-    <div>
-      <ul>
-        <li> Pricing</li>
-        <li> About</li>
-        <li> Contact</li>
-      </ul>
-    </div>
-  </nav>
+const page = (
+  <div>
+    <img src="./assets/img/react-logo.svg" width="40" />
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousand of enterprise apps, including mobile apps</li>
+    </ul>
+  </div>
 )
 
-console.log("declarative code", navbar)
-ReactDOM.render(
-  navbar, document.getElementById("root"))
-
-const header = document.createElement('h1')
-header.textContent = "Heading 1 with imperative programming"
-document.getElementById('root').append(header)
-console.log("imperative code", header)
+ReactDOM.render(page, document.getElementById('root'))
