c212b60798c15707bfa683252597eb52d67cdb0a
Sinta Anjelina <sinta.anjelina3930@gmail.com>
2022-02-18 23:18:28 +0700

    Create other custom components
    In react, custom component is like a function that
    consists of ui elements in JSX structure that
    could be called over and over again
    to add it into real DOM.
    Its name is not written in camel case but in Capital Cases.
    it needs to be nested under single parent element.
    It is called by using tag for example:
    <Section1 />

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-const page = (
-  <div>
-    <img src="./assets/img/react-logo.svg" width="40" />
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100K stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousand of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
-)
+const Section1 = () => {
+  return (
+    <div>
+      <img src="./assets/img/react-logo.svg" width="40" />
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousand of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  )
+}
 
-ReactDOM.render(page, document.getElementById('root'))
+const Section2 = () => {
+  return (
+    <React.Fragment>
+      <h1> Reasons why i learn react </h1>
+      <ol>
+        <li> Build my portofolio </li>
+        <li> To be qualified on job i wanted to apply</li>
+        <li> To learn and understand Javascript more</li>
+        <li> To understand at least one of Javascript Frontend Framework</li>
+      </ol>
+    </React.Fragment>
+  )
+}
+
+ReactDOM.render(<div><Section1 /><Section2 /></div >, document.getElementById('root'))  
