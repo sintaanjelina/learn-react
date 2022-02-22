5a32e4dce55d1e79c680b30b4c4908ce37fc4eb9
Sinta Anjelina <sinta.anjelina3930@gmail.com>
2022-02-22 15:49:38 +0700

    Create Navbar react component for app index

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Navbar from "./components/navbar";
+
+export default function App() {
+    return (
+        <div className="app--container">
+            <Navbar />
+        </div>
+    )
+}
