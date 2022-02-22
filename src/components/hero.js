8aa2789764d2e4a9d1aa57848ea3fc7aed79e2a1
Sinta Anjelina <sinta.anjelina3930@gmail.com>
2022-02-22 23:46:59 +0700

    styling css of hero section component

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,12 +3,12 @@ import photo_grid from "../images/photo-grid.png"
 
 export default function Hero() {
     return (
-        <div className="hero">
+        <section className="hero">
             <img className="hero--image" src={photo_grid} />
-            <h2 className="hero--title">Online Experiences</h2>
+            <h1 className="hero--title">Online Experiences</h1>
             <p className="hero--text">
                 Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
             </p>
-        </div>
+        </section>
     )
 }
